e37a4f71bc74dbebea904b6bdd9c6baad3f22685
Enrico R. Crema <enrico.crema@gmail.com>
2019-04-24 10:04:40 +0100

    Fixed material Code inconsistencies

--- R_scripts/clean_and_translate.R
+++ R_scripts/clean_and_translate.R
@@ -8,7 +8,7 @@ setwd("~/gitrepos/ENCOUNTER/encounter_c14db/")
 #### Clean Kudo's Original Table ####
 
 ## Read Data
-c14raw = read_xlsx("./raw_data/★年代測定データベース学術・関東･東北・北陸・中部・鹿児島181031.xlsx",skip=1)
+c14raw = read_xlsx("./raw_data/★年代測定データベース学術・関東･東北・北陸・中部・鹿児島190423.xlsx",skip=1)
 c14raw = as.data.frame(c14raw)
 
 ## Assign New Columns Names
@@ -92,34 +92,6 @@ if (!all(c14db$MaterialCode2%in%matcode2$Code))
 	c14db$MaterialCode2[j]
 }
 
-
-#### ISSUES
-
-## 1:Specimen 22263 is recorded with lower case for MaterialCode1 but should be upper case.
-c14db$MaterialCode1[i[2]]
-
-#Fix: change to upper case
-
-## 2:Speciments 21704, 28099, 28227 are NA
-c14db$MaterialCode1[i[-2]]
-
-#Fix: 21704 is recorded in MaterialType as 炭化材(ケヤキ), and hence should have MaterialCode1 as "T",.and MaterialCode2 as "d";28099 and 28277 has no information so should be left as NA?
-
-## 3 Several entries have different encoding for "n" in MaterialCode2
-j[which(c14db$MaterialCode2[j]=="ｎ")]
-
-#Fix: recode the terms
-
-## 4 Several entries have NA in MaterialCode2. 
-(k=j[which(is.na(c14db$MaterialCode2[j]))])
-
-#Fix: 21704 should be coded as "d" (se above"), 25600 should be coded as "d"?, and 27001 as "a","b", or "o"? Suggest perhaps a generic bone category?
-
-## 5 Several entries are labelled "e,g":
-
-j[which(c14db$MaterialCode2[j]=="e,g")]
-
-#Fix: recode using just g?
 
 
 
