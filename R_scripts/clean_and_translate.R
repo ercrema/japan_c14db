15684c43b515e0e7e86aaeac6cfb104346cdc5b2
Enrico R. Crema <enrico.crema@gmail.com>
2019-06-30 15:57:14 +0100

    Added spatial coordinates check

--- R_scripts/clean_and_translate.R
+++ R_scripts/clean_and_translate.R
@@ -2,6 +2,9 @@
 library(readxl)
 library(measurements)
 library(sp)
+library(NipponMap) 
+library(mapdata)
+library(maptools)
 
 setwd("~/gitrepos/ENCOUNTER/encounter_c14db/")
 
@@ -62,7 +65,55 @@ c14db = c14raw
 c14db$Latitude=as.numeric(char2dms(paste0(c14db$Latitude,"N"),chd='゜',chm="'"))
 c14db$Longitude=as.numeric(char2dms(paste0(c14db$Longitude,"E"),chd='゜',chm="'"))
 
+#### Extract prefecture names 
+pref = read.csv("~/gitrepos/ENCOUNTER/encounter_c14db/processed_data/prefectures_translations.csv")
+colnames(pref) = c("Prefecture","PrefectureNameEn","Region")
+
+c14db=plyr::join(c14db,pref,type='left')
+
 # Add Here Scripts for checking site coordinates
+sitecoord = data.frame(RowN=3:(nrow(c14db)+2),
+		       PrefectureCode=c14db$PrefectureCode,
+		       PrefectureName=c14db$PrefectureNameEn,
+		       SiteName=c14db$SiteName,
+		       SiteLocation=c14db$SiteLocation,
+		       Latitude=round(c14db$Latitude,4),
+		       Longitude=round(c14db$Longitude,4))
+sitecoord = unique(sitecoord)
+
+### Are sites within Japan?
+
+jwgs84 = rgdal::readOGR(dsn="~/gitrepos/ENCOUNTER/encounter_c14db/gis",layer='japan_wgs84')
+
+jjgd2000 = rgdal::readOGR(dsn="~/gitrepos/ENCOUNTER/encounter_c14db/gis",layer='japan_jgd2000')
+
+site_wgs84 = sitecoord
+site_jgd2000 = sitecoord
+coordinates(site_wgs84) <- c("Longitude","Latitude")
+coordinates(site_jgd2000) <- c("Longitude","Latitude")
+proj4string(site_wgs84) <- proj4string(jwgs84)
+proj4string(site_jgd2000) <- proj4string(jjgd2000)
+
+sitecoord$extractedWGS84=over(site_wgs84,jwgs84)$prefecture
+sitecoord$extractedJGD2000=over(site_jgd2000,jjgd2000)$prefecture
+
+#Mismatch sites
+# j = which(as.character(sitecoord$extractedWGS84)!=as.character(sitecoord$extractedJGD2000))
+# length(j)
+
+
+i = which(sitecoord$PrefectureName!=as.character(sitecoord$extractedWGS84)) #mismatching sites
+
+k = which(is.na(sitecoord$extractedWGS84)) # sites with no coordinates
+
+mismatched=sitecoord[c(i,k),]
+
+write.csv(mismatched,"~/gitrepos/ENCOUNTER/encounter_c14db/problems/mismatchsite.csv")
+
+
+
+
+
 
 ####  Translate Methods
 # Method
@@ -91,7 +142,6 @@ if (!all(c14db$MaterialCode2%in%matcode2$Code))
 	c14db[j,]
 	c14db$MaterialCode2[j]
 }
-
 
 
 
