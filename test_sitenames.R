53f2c70eec28789135cb771cbfcb2b8c6bb61ead
Enrico R. Crema <enrico.crema@gmail.com>
2020-06-09 08:29:34 +0100

    Remove all deprecated files before version 0.1.0

--- test_sitenames.R
+++
@@ -1,9 +0,0 @@
-load("./runc14raw.RData")
-source("./utility/siteNameTranscript.R")
-siteList = unique(c14raw$SiteName)
-sitenames=siteNameTranscript(siteList)
-#Export for manual editing
-sitenames$matched=(sitenames$SiteName==sitenames$MatchedName)
-sitenames$ToCheck = !sitenames$matched | is.na(sitenames$Furigana)
-sitenames=dplyr::select(sitenames,SiteName,Furigana,ToCheck)
-write.csv(sitenames,"./lookup/sitenamesTranscript.csv",row.names = FALSE)
